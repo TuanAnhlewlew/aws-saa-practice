168ddc091ec878f0a2d176fb5b38b90eb92b4747
Vũ Tuấn Anh <73298009+TuanAnhlewlew@users.noreply.github.com>
2025-01-13 07:25:15 +0000

    add ignore

---
+++ setup.sh
@@ -0,0 +1,9 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo apt jq
+
+sudo apt tree
+
+export AWS_CLI_AUTO_PROMPT=on-partial
